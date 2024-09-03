d4a41392d6931b1dd0f863dbf400c692d929ee09
Maximilian Vogt <vogtmh@gmail.com>
2024-09-03 17:25:40 +0700

    modularize settings

--- subwayTube/js/main.js
+++ subwayTube/js/main.js
@@ -6,7 +6,7 @@ var subscriptions = [];
 var playhistory = [];
 var searchhistory = [];
 var downloadhistory = [];
-var tab = 'trending';
+var tab = 'feed';
 var selectedFolder;
 var downloadFolder;
 var videoActive = false;
@@ -183,7 +183,7 @@ function stopTouch(e) {
 }
 
 function showFeed() {
-    tab = 'trending';
+    tab = 'feed';
     let timenow = (new Date).getTime();
     let feedage = timenow - feedtimestamp;
     if (feedage > 1800000) {
@@ -237,7 +237,7 @@ function checkMirror() {
         },
         error(jqXHR, status, errorThrown) {
             console.log('[Mirror] offline')
-            if (tab == 'trending') {
+            if (tab == 'feed') {
                 let html = '<div style="margin-top: 2em; font-size:2em">Could not connect to server. Please choose another mirror from the settings.</div>';
                 $('#feed').html(html);
             }
@@ -247,7 +247,7 @@ function checkMirror() {
 
 function getFeed(trycount) {
     checkMirror(); 
-    if (tab == 'trending') {
+    if (tab == 'feed') {
         printFeedHeader(trycount)
     }
 
@@ -279,7 +279,7 @@ function getFeed(trycount) {
                     html += `<div class="videoitem" onclick='playVideo("` + videoId + `", 1)'><img src="` + image + `"/><div class="videoinfo">` + shortStr(title) + '<br/>' + author + '<br/>' + published + `</div></div>`;
                 }
                 html += '<div id="spacer"></div>'
-                if (tab == 'trending') {
+                if (tab == 'feed') {
                     $('#feed').html(html);
                 }
                 applySizing();
@@ -301,7 +301,7 @@ function getFeed(trycount) {
 }
 
 function printFeed(trycount) {
-    if (tab == 'trending') {
+    if (tab == 'feed') {
         printFeedHeader()
         $('#feed').html(feedcontent);
         applySizing();
@@ -684,10 +684,6 @@ function clearSharetext() {
     $("#sharetext").html('')
 }
 
-function clearSettingstext() {
-    $("#settingstext").html('')
-}
-
 function toggleChannel(authorId, author, authorThumbnail, origin) {
     if (isSubscribed(authorId)) {
         removeChannel(authorId, origin);
@@ -828,10 +824,6 @@ function addDownloadhistoryItem(videoFile, name, image, author, authorId, author
             localStorage.downloadhistory = JSON.stringify(downloadhistory);
         });
     });
-
-    
-
-    
 }
 
 function removeDownload(fileName) {
@@ -876,16 +868,6 @@ function removeDownload(fileName) {
 
     loadDownloadHistory()
     getDownloads()
-}
-
-function clearHistory() {
-    playhistory = [];
-    searchhistory = [];
-    localStorage.playhistory = JSON.stringify(playhistory);
-    localStorage.searchhistory = JSON.stringify(searchhistory);
-    console.log('History has been cleared.')
-    $("#settingstext").html('History has been cleared.')
-    setTimeout(clearSettingstext, 3000)
 }
 
 function followChannel(authorId, author, authorThumbnail, origin) {
@@ -1727,119 +1709,11 @@ function showChannelStreams(id) {
     });
 }
 
-
 function closeChannel() {
     $("#channelviewer").hide();
     $("#channelheader").html("");
     $("#channeltablist").html("");
     $("#channelcontent").html('<img src="images/loading.gif" />')
-}
-
-function getServerlist() {
-    let requesturl = 'https://api.invidious.io/instances.json?pretty=1&sort_by=health'
-
-    $.ajax({
-        url: requesturl,
-        type: 'GET',
-        dataType: 'json',
-        success(response) {
-            serverlist = {}
-            var html = `<label for="servers">Server:</label><br/>
-                        <select name="servers" id="servers" onchange="applySettings()">`;
-            var stats;
-            for (var i = 0; i < response.length; i++) {
-                var element = response[i];
-                let servername = element[0]
-                let serverurl = 'https://'+servername
-                let attributes = element[1]
-                let type = attributes.type;
-                let cors = attributes.cors;
-                let api = attributes.api;
-                
-                if (type == 'https' && api == true && cors == true) {
-                    try {
-                        if (attributes.region) {
-                            let region = attributes.region;
-                        }
-                        if (attributes.monitor) {
-                            let uptime = attributes.monitor.uptime
-                        }
-                        if (server == serverurl) {
-                            html += '<option value="' + servername + '" selected>' + servername + '</option>'
-                        }
-                        else {
-                            html += '<option value="' + servername + '">' + servername + '</option>'
-                        }
-                        serverlist[servername] = { "name": servername, "url": serverurl, "attributes": attributes }
-                    }
-                    catch (e) {
-                        console.log(element);
-                        console.log(e.message);
-                    }
-                }  
-            }
-
-            html += `</select>`
-            if (tab == 'settings') {
-                $("#setting_serverlist").html(html);
-                console.log('[Serverlist] updated.')
-                showServerstats()
-            }
-        },
-        error(jqXHR, status, errorThrown) {
-            console.log('failed to fetch ' + requesturl)
-        },
-    });
-}
-
-function showServerstats() {
-    try {
-        let servername = $("#servers").val()
-        let attributes = serverlist[servername].attributes
-        let region = attributes.region;
-        let uptime = attributes.monitor.uptime;
-        stats = 'Location: ' + region + ', uptime: ' + uptime;
-        if (tab == 'settings') {
-            $("#setting_serverstats").html(stats);
-        }
-    }
-    catch (e) {
-        $("#setting_serverstats").html('Cannot display serverinfo. Serverlist is unavailable or empty.');
-    }
-}
-
-function showSettings() {
-    tab = 'settings';
-    $("#topsettings").hide();
-    $(".navbutton").css("background-color", "#111");
-    $("#nav_settings").css("background-color", "Highlight");
-
-    var output = `<h2 style="text-align: left;">Settings</h2>
-                  <div style="text-align: left; line-height: 150%;">
-                    <div>subwayTube ` + appstring + ` by mavodev <br/> powered by <a href="https://invidious.io/" target="_blank">Invidious</a></div>
-                    <div id="setting_serverlist"></div>
-                    <div id="setting_serverstats"></div>
-                    <div style="width: 100%;height:30px"></div>
-                    <table style="width:100%;">
-                    <tr>
-                      <td style="width:33%"><div class="settingsbutton" id="backupbutton" onclick='createBackup()'><img src="images/backup.png" /><br/>Backup</div></td>
-                      <td><div class="settingsbutton" id="restorebutton" onclick='restoreBackup()'><img src="images/restore.png" /><br/>Restore</div></td>
-                      <td style="width:33%"><div class="settingsbutton" id="clearbutton" onclick='clearHistory()'><img src="images/clean.png" /><br/>Clear history</div></td>
-                    </tr>
-                    </table>
-               
-                    <div id="settingstext"></div>
-                    <div id="downloadpath">Download folder: <br/>`+ downloadFolder +` <button onclick="selectDownloadPath()">Change</button></div>
-                    <div id="streamquality"></div>
-                    <div id="applybutton" style='display:none;' onclick='applySettings()'>Apply</div>
-                  </div>`
-    if (tab == 'settings') {
-        $('#content').html(output);
-        //getDownloads();
-    }
-
-    getServerlist()
-    getStreamquality()
 }
 
 function convertImgToBase64(url, callback, outputFormat) {
@@ -1893,139 +1767,6 @@ function printDownload(fileName, name, image, author, authorId, authorThumbnail,
             }
         });
     });
-}
-
-function getStreamquality() {
-    let qualities = ['144p', '360p', '720p', '1080p'];
-
-    let output = `<label for="streamqualityselect">Stream Quality:</label>
-                  <select name="streamqualityselect" id="streamqualityselect" onchange="applySettings()">`;
-    for (let i = 0; i < qualities.length; i++) {
-        let quality = qualities[i];
-        let qualitytext = quality + ' (audio issues)';
-        if (quality == '360p') {
-            qualitytext = quality + ' (recommended)';
-        }
-        if (quality == streamquality) {
-            output += '<option value="' + quality + '" selected>' + qualitytext + '</option>'
-        }
-        else {
-            output += '<option value="' + quality + '">' + qualitytext + '</option>'
-        }
-    }
-    output += '</select>';
-    $("#streamquality").html(output); 
-}
-
-function createBackup() {
-    var now = new Date();
-    var datestamp = ("0" + now.getDate()).slice(-2) + "-" + ("0" + (now.getMonth() + 1)).slice(-2) + "-" + now.getFullYear();
-    var backupJSON = {};
-    backupJSON.info = 'subwayTube backup';
-    backupJSON.date = datestamp;
-    backupJSON.subscriptions = subscriptions;
-    backupJSON.playhistory = playhistory;
-
-    var savePicker = new Windows.Storage.Pickers.FileSavePicker();
-    savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
-    savePicker.fileTypeChoices.insert("JSON", [".json"]);
-    savePicker.suggestedFileName = "subwaytube_backup";
-
-    savePicker.pickSaveFileAsync().then(function (file) {
-        if (file != null) {
-            let jsonstring = JSON.stringify(backupJSON);
-            Windows.Storage.FileIO.writeTextAsync(file, jsonstring).done(function () {
-                Windows.Storage.CachedFileManager.completeUpdatesAsync(file).done(function (updateStatus) {
-                    if (updateStatus === Windows.Storage.Provider.FileUpdateStatus.complete) {
-                        console.log("File " + file.name + " was saved.");
-                        $("#settingstext").html("File " + file.name + " was saved.");
-                        setTimeout(clearSettingstext, 3000)
-                    } else {
-                        console.log("File " + file.name + " couldn't be saved.");
-                        $("#settingstext").html("File " + file.name + " couldn't be saved.");
-                        setTimeout(clearSettingstext, 3000)
-                    }
-                });
-            });
-        }
-        else {
-            console.log("Canceled");
-        }
-
-    });
-}
-
-function selectBackupfile() {
-    var picker = new Windows.Storage.Pickers.FileOpenPicker();
-    picker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
-    picker.fileTypeFilter.append(".json");
-    return picker.pickSingleFileAsync();
-}
-
-function restoreBackup() {
-    selectBackupfile().then(function (file) {
-        try {
-            Windows.Storage.FileIO.readTextAsync(file).then(function (fileContent) {
-                var importedChannels = 0;
-                var importedHistory = 0;
-                var importArray = JSON.parse(fileContent);
-
-                var fileinfo = importArray.info;
-                var filedate = importArray.date;
-
-                if (fileinfo != 'microTube backup' && fileinfo != 'subwayTube backup') {
-                    $("#settingstext").html('No valid file. Only subwayTube exports can be restored.');
-                    setTimeout(clearSettingstext, 3000)
-                    return;
-                }
-
-                var filesubs = importArray.subscriptions;
-
-                for (let i = 0; i < filesubs.length; i++) {
-                    channel = filesubs[i];
-                    authorId = channel.authorId
-                    author = channel.author
-                    authorThumbnail = channel.image
-                    followChannel(authorId, author, authorThumbnail)
-                    importedChannels++
-                }
-
-                var historybackup = importArray.playhistory;
-
-                for (let i = 0; i < historybackup.length; i++) {
-                    historyitem = historybackup[i];
-                    id = historyitem.id;
-                    name = historyitem.name;
-                    image = historyitem.image;
-                    authorId = historyitem.authorId
-                    author = historyitem.author
-                    
-                    addHistoryItem(id, name, image, authorId, author);
-                    importedHistory++
-                }
-                let message = 'Restored: ' + importedChannels + ' channels and '+ importedHistory + ' history from ' + filedate;
-                $("#settingstext").html(message);
-                setTimeout(clearSettingstext, 3000)
-            });
-        } catch (err) {
-            console.error(err);
-        }
-    });
-}
-
-function hideSettings() {
-    $("#settingsmenu").hide();
-}
-
-function applySettings() {
-    let servername = $("#servers").val()
-    localStorage.invidious_server = 'https://'+servername;
-    server = localStorage.invidious_server;
-    showServerstats()
-    let quality = $("#streamqualityselect").val()
-    localStorage.streamquality = quality;
-    streamquality = localStorage.streamquality;
-    showServerstats()
 }
 
 // prevents that each back button press will suspend the app
@@ -2112,18 +1853,6 @@ function fileExists(folder, fileName) {
     });
 }
 
-/*
-function saveFileToFolder(folder, fileUrl, fileName) {
-    return downloadFile(fileUrl).then(function (fileData) {
-        return fileData;
-    }).then(function (fileData) {
-        return folder.createFileAsync(fileName, Windows.Storage.CreationCollisionOption.generateUniqueName).then(function (file) {
-            return Windows.Storage.FileIO.writeBufferAsync(file, fileData);
-        });
-    });
-}
-*/
-
 function saveFileToFolder(folder, fileUrl, fileName) {
     return fileExists(folder, fileName).then(function (exists) {
         if (!exists) {
@@ -2137,7 +1866,6 @@ function saveFileToFolder(folder, fileUrl, fileName) {
         }
     });
 }
-
 
 function downloadVideo(fileUrl, fileName, name, image, author, authorId, authorThumbnail) {
     fileName = fileName.replace(/ /g, "_");
---
+++ subwayTube/js/settings.js
@@ -0,0 +1,253 @@
+﻿function showSettings() {
+    tab = 'settings';
+    $("#topsettings").hide();
+    $(".navbutton").css("background-color", "#111");
+    $("#nav_settings").css("background-color", "Highlight");
+
+    var output = `<h2 style="text-align: left;">Settings</h2>
+                  <div style="text-align: left; line-height: 150%;">
+                    <div>subwayTube ` + appstring + ` by mavodev <br/> powered by <a href="https://invidious.io/" target="_blank">Invidious</a></div>
+                    <div id="setting_serverlist"></div>
+                    <div id="setting_serverstats"></div>
+                    <div style="width: 100%;height:30px"></div>
+                    <table style="width:100%;">
+                    <tr>
+                      <td style="width:33%"><div class="settingsbutton" id="backupbutton" onclick='createBackup()'><img src="images/backup.png" /><br/>Backup</div></td>
+                      <td><div class="settingsbutton" id="restorebutton" onclick='restoreBackup()'><img src="images/restore.png" /><br/>Restore</div></td>
+                      <td style="width:33%"><div class="settingsbutton" id="clearbutton" onclick='clearHistory()'><img src="images/clean.png" /><br/>Clear history</div></td>
+                    </tr>
+                    </table>
+               
+                    <div id="settingstext"></div>
+                    <div id="downloadpath">Download folder: <br/>`+ downloadFolder + ` <button onclick="selectDownloadPath()">Change</button></div>
+                    <div id="streamquality"></div>
+                    <div id="applybutton" style='display:none;' onclick='applySettings()'>Apply</div>
+                  </div>`
+    if (tab == 'settings') {
+        $('#content').html(output);
+        //getDownloads();
+    }
+
+    getServerlist()
+    getStreamquality()
+}
+
+function hideSettings() {
+    $("#settingsmenu").hide();
+}
+
+function applySettings() {
+    let servername = $("#servers").val()
+    localStorage.invidious_server = 'https://' + servername;
+    server = localStorage.invidious_server;
+    showServerstats()
+    let quality = $("#streamqualityselect").val()
+    localStorage.streamquality = quality;
+    streamquality = localStorage.streamquality;
+    showServerstats()
+}
+
+function clearSettingstext() {
+    $("#settingstext").html('')
+}
+
+function getServerlist() {
+    let requesturl = 'https://api.invidious.io/instances.json?pretty=1&sort_by=health'
+
+    $.ajax({
+        url: requesturl,
+        type: 'GET',
+        dataType: 'json',
+        success(response) {
+            serverlist = {}
+            var html = `<label for="servers">Server:</label><br/>
+                        <select name="servers" id="servers" onchange="applySettings()">`;
+            var stats;
+            for (var i = 0; i < response.length; i++) {
+                var element = response[i];
+                let servername = element[0]
+                let serverurl = 'https://' + servername
+                let attributes = element[1]
+                let type = attributes.type;
+                let cors = attributes.cors;
+                let api = attributes.api;
+
+                if (type == 'https' && api == true && cors == true) {
+                    try {
+                        if (attributes.region) {
+                            let region = attributes.region;
+                        }
+                        if (attributes.monitor) {
+                            let uptime = attributes.monitor.uptime
+                        }
+                        if (server == serverurl) {
+                            html += '<option value="' + servername + '" selected>' + servername + '</option>'
+                        }
+                        else {
+                            html += '<option value="' + servername + '">' + servername + '</option>'
+                        }
+                        serverlist[servername] = { "name": servername, "url": serverurl, "attributes": attributes }
+                    }
+                    catch (e) {
+                        console.log(element);
+                        console.log(e.message);
+                    }
+                }
+            }
+
+            html += `</select>`
+            if (tab == 'settings') {
+                $("#setting_serverlist").html(html);
+                console.log('[Serverlist] updated.')
+                showServerstats()
+            }
+        },
+        error(jqXHR, status, errorThrown) {
+            console.log('failed to fetch ' + requesturl)
+        },
+    });
+}
+
+function showServerstats() {
+    try {
+        let servername = $("#servers").val()
+        let attributes = serverlist[servername].attributes
+        let region = attributes.region;
+        let uptime = attributes.monitor.uptime;
+        stats = 'Location: ' + region + ', uptime: ' + uptime;
+        if (tab == 'settings') {
+            $("#setting_serverstats").html(stats);
+        }
+    }
+    catch (e) {
+        $("#setting_serverstats").html('Cannot display serverinfo. Serverlist is unavailable or empty.');
+    }
+}
+
+function getStreamquality() {
+    let qualities = ['144p', '360p', '720p', '1080p'];
+
+    let output = `<label for="streamqualityselect">Stream Quality:</label>
+                  <select name="streamqualityselect" id="streamqualityselect" onchange="applySettings()">`;
+    for (let i = 0; i < qualities.length; i++) {
+        let quality = qualities[i];
+        let qualitytext = quality + ' (audio issues)';
+        if (quality == '360p') {
+            qualitytext = quality + ' (recommended)';
+        }
+        if (quality == streamquality) {
+            output += '<option value="' + quality + '" selected>' + qualitytext + '</option>'
+        }
+        else {
+            output += '<option value="' + quality + '">' + qualitytext + '</option>'
+        }
+    }
+    output += '</select>';
+    $("#streamquality").html(output);
+}
+
+function createBackup() {
+    var now = new Date();
+    var datestamp = ("0" + now.getDate()).slice(-2) + "-" + ("0" + (now.getMonth() + 1)).slice(-2) + "-" + now.getFullYear();
+    var backupJSON = {};
+    backupJSON.info = 'subwayTube backup';
+    backupJSON.date = datestamp;
+    backupJSON.subscriptions = subscriptions;
+    backupJSON.playhistory = playhistory;
+
+    var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+    savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
+    savePicker.fileTypeChoices.insert("JSON", [".json"]);
+    savePicker.suggestedFileName = "subwaytube_backup";
+
+    savePicker.pickSaveFileAsync().then(function (file) {
+        if (file != null) {
+            let jsonstring = JSON.stringify(backupJSON);
+            Windows.Storage.FileIO.writeTextAsync(file, jsonstring).done(function () {
+                Windows.Storage.CachedFileManager.completeUpdatesAsync(file).done(function (updateStatus) {
+                    if (updateStatus === Windows.Storage.Provider.FileUpdateStatus.complete) {
+                        console.log("File " + file.name + " was saved.");
+                        $("#settingstext").html("File " + file.name + " was saved.");
+                        setTimeout(clearSettingstext, 3000)
+                    } else {
+                        console.log("File " + file.name + " couldn't be saved.");
+                        $("#settingstext").html("File " + file.name + " couldn't be saved.");
+                        setTimeout(clearSettingstext, 3000)
+                    }
+                });
+            });
+        }
+        else {
+            console.log("Canceled");
+        }
+
+    });
+}
+
+function selectBackupfile() {
+    var picker = new Windows.Storage.Pickers.FileOpenPicker();
+    picker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
+    picker.fileTypeFilter.append(".json");
+    return picker.pickSingleFileAsync();
+}
+
+function restoreBackup() {
+    selectBackupfile().then(function (file) {
+        try {
+            Windows.Storage.FileIO.readTextAsync(file).then(function (fileContent) {
+                var importedChannels = 0;
+                var importedHistory = 0;
+                var importArray = JSON.parse(fileContent);
+
+                var fileinfo = importArray.info;
+                var filedate = importArray.date;
+
+                if (fileinfo != 'microTube backup' && fileinfo != 'subwayTube backup') {
+                    $("#settingstext").html('No valid file. Only subwayTube exports can be restored.');
+                    setTimeout(clearSettingstext, 3000)
+                    return;
+                }
+
+                var filesubs = importArray.subscriptions;
+
+                for (let i = 0; i < filesubs.length; i++) {
+                    channel = filesubs[i];
+                    authorId = channel.authorId
+                    author = channel.author
+                    authorThumbnail = channel.image
+                    followChannel(authorId, author, authorThumbnail)
+                    importedChannels++
+                }
+
+                var historybackup = importArray.playhistory;
+
+                for (let i = 0; i < historybackup.length; i++) {
+                    historyitem = historybackup[i];
+                    id = historyitem.id;
+                    name = historyitem.name;
+                    image = historyitem.image;
+                    authorId = historyitem.authorId
+                    author = historyitem.author
+
+                    addHistoryItem(id, name, image, authorId, author);
+                    importedHistory++
+                }
+                let message = 'Restored: ' + importedChannels + ' channels and ' + importedHistory + ' history from ' + filedate;
+                $("#settingstext").html(message);
+                setTimeout(clearSettingstext, 3000)
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    });
+}
+
+function clearHistory() {
+    playhistory = [];
+    searchhistory = [];
+    localStorage.playhistory = JSON.stringify(playhistory);
+    localStorage.searchhistory = JSON.stringify(searchhistory);
+    console.log('History has been cleared.')
+    $("#settingstext").html('History has been cleared.')
+    setTimeout(clearSettingstext, 3000)
+}
